c8704d8bb993730b7806325362f99bb68691c50a
julgoetz <70372980+jugoetz@users.noreply.github.com>
2021-04-19 18:02:29 +0200

    reformat xlsx output

--- src/weigh-in-calculator.py
+++ src/weigh-in-calculator.py
@@ -1,6 +1,7 @@
 from labware.plates import Plate96, Plate384
 from pathlib import Path
 import pickle as pkl
+import json
 import re
 import os
 import xlsxwriter
@@ -24,8 +25,8 @@ COMPOUND_MAPPING = OUTPUT_DIR / 'compound_mapping.txt'
 with open(OUTPUT_DIR / 'library_constituents_dataframe.pkl', 'rb') as file:
     df = pkl.load(file)
 
-with open(OUTPUT_DIR / 'synthesis_plan.pkl', 'rb') as file:
-    synthesis_plan = pkl.load(file)
+with open(OUTPUT_DIR / 'synthesis_plan.json', 'r') as file:
+    synthesis_plan = json.load(file)
 
 mapping = {}
 with open(OUTPUT_DIR / 'compound_mapping.txt', 'r') as file:
@@ -59,31 +60,44 @@ for k, v in compound_volumes.items():
     v.append(mass)
 
 # flatten dict to list for convenience with the output
-cmp_list = [[k]+v for k, v in compound_volumes.items()]
+cmp_list = [[k] + v for k, v in compound_volumes.items()]
 # output
 workbook = xlsxwriter.Workbook(EXP_DIR / 'weigh-in.xlsx')
 worksheet = workbook.add_worksheet()
+decimal_format_two_places = workbook.add_format()
+decimal_format_two_places.set_num_format(2)
+decimal_format_int = workbook.add_format()
+decimal_format_int.set_num_format(1)
+
 worksheet.write(0, 0, 'Short')
 worksheet.write(0, 1, 'Long')
-worksheet.write(0, 2, 'V [uL]')
+worksheet.write(0, 2, 'Barcode')
 worksheet.write(0, 3, 'theor. m [mg]')
 worksheet.write(0, 4, 'actual m [mg]')
-worksheet.write(0, 5, 'actual V [uL]')
+worksheet.write(0, 5, 'V [uL]')
+worksheet.write(0, 6, 'actual V [uL]')
+worksheet.write(0, 7, 'V DMSO [uL]')
+worksheet.write(0, 8, 'V oxalic [uL]')
 col = 0
 for i, cmp in enumerate(cmp_list):
     row = i + 1  # +1 for header
     a = cmp[0]
     b = cmp[1]
-    c = cmp[2]
+    f = cmp[2]
     d = round(cmp[3], 2)
 
-    f = f'=E{row + 1}/D{row + 1}*C{row + 1}'  # +1 for excel convention to start at 1
+    g = f'=E{row + 1}/D{row + 1}*F{row + 1}'  # +1 for excel convention to start at 1
+    h = f'=G{row + 1}*0.9'
+    i = f'=G{row + 1}*0.1'
 
     worksheet.write(row, col, a)
     worksheet.write(row, col + 1, b)
-    worksheet.write(row, col + 2, c)
-    worksheet.write(row, col + 3, d)
-    worksheet.write(row, col + 5, f)
+    worksheet.write(row, col + 3, d, decimal_format_two_places)
+    worksheet.write(row, col + 4, None, decimal_format_two_places)
+    worksheet.write(row, col + 5, f, decimal_format_int)
+    worksheet.write(row, col + 6, g, decimal_format_int)
+    worksheet.write(row, col + 7, h, decimal_format_int)
+    worksheet.write(row, col + 8, i, decimal_format_int)
 
 
 workbook.close()

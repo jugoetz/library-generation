203f60459df64fa1865a961c1ca7d089a5312a83
julgoetz <70372980+jugoetz@users.noreply.github.com>
2021-06-03 11:38:32 +0200

    correct pathes

--- notebooks/notebook_config.py
+++ notebooks/notebook_config.py
@@ -3,14 +3,13 @@ from pathlib import Path
 
 # directories and files
 DATA_DIR = Path('..', 'data').resolve()
-OUTPUT_DIR = DATA_DIR / 'outputs'
 INPUT_DIR = DATA_DIR / 'inputs'
 SDF_DIR = DATA_DIR / 'library_static'
 DB_PATH = DATA_DIR / 'db' / '50k_project.db'
 DB_STATIC_DIR = DATA_DIR / 'db' / 'static'
 SUBMISSION_FORM_TEMPLATE = DATA_DIR / 'util' / 'BMIIyyyyyy-SampleTable_JGxxx.xls'
 EXP_NR = 'JG216'  # this will change frequently
-EXP_DIR = OUTPUT_DIR / 'target_plates' / EXP_NR
+EXP_DIR = DATA_DIR / 'plates' / EXP_NR
 
 # debugging
 DEBUG = 1
@@ -18,7 +17,7 @@ verbose = True
 
 # Library things
 # COMPOUND_MAPPING_PATH = EXP_DIR / 'identity.txt'
-COMPOUND_MAPPING_PATH = OUTPUT_DIR / 'compound_mapping.txt'
+COMPOUND_MAPPING_PATH = DATA_DIR / 'buildingblocks' / 'compound_mapping.txt'
 # PLATE_REGEX = re.compile('test_JG([0-9]+).csv')
 PLATE_REGEX = re.compile('plate_layout_plate([0-9]+).csv')
 

71341fb35cf8ec9f6dba58e8be50ce2d43bb4d15
julgoetz <70372980+jugoetz@users.noreply.github.com>
2021-06-03 11:23:50 +0200

    1st upload

---
+++ src/db_models/models_base.py
@@ -0,0 +1,93 @@
+"""
+Base models to interact with the database.
+
+To extend functionality, subclass these models in models_custom.py
+"""
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from rdkit import Chem
+
+Base = declarative_base()
+
+
+class BuildingBlockBase(Base):
+    """
+    A building block has few necessary _input_ properties:
+        - name (unique)
+        - SMILES
+        - an nickname for the context of the HTLibrary (unique, can be user-defined or auto-generated)
+        - a reactant class (can be "generic")
+    A few things will always be generated:
+        - id (unique, generated in DB)
+        - molblock representation of the building block
+        - bottles: Reference to bottles containing this
+
+    """
+    __tablename__ = 'buildingblock'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    smiles = Column(String)
+    nickname = Column(String)
+    reactant_class = Column(String)
+    molblock = Column(String)
+    customization = Column(String)
+
+    bottles = relationship('BottleBase', back_populates='buildingblock')
+
+    __mapper_args__ = {
+        'polymorphic_on':       customization,
+        'polymorphic_identity': 'buildingblockbase'
+    }
+
+    def __init__(self,
+                 name,
+                 smiles,
+                 nickname='auto',
+                 reactant_class='generic',
+                 id_int=None,
+                 ):
+        self.name = name
+        self.smiles = smiles
+        self.nickname = nickname
+        self.reactant_class = reactant_class
+        self.id = id_int
+        self.mol = self.__get_mol()
+
+    def __repr__(self):
+        return f'BuildingBlock(id={self.id}, name={self.name}, smiles={self.smiles})'
+
+    def __get_mol(self):
+        """Return a molblock from SMILES"""
+        mol = Chem.MolFromSmiles(self.smiles)
+        Chem.SanitizeMol(mol)
+        return Chem.MolToMolBlock(mol)
+
+
+class BottleBase(Base):
+    """
+    Base class for chemical bottles.
+    A bottle needs at least:
+    - id (unique primary key)
+    - buildingblock_id (ForeignKey referencing BuildingBlock.id)
+    - barcode (nullable)
+    -
+    """
+    __tablename__ = 'bottle'
+
+    id = Column(Integer, primary_key=True)
+    buildingblock_id = Column(Integer, ForeignKey('buildingblock.id'))
+    barcode = Column(Integer)
+    customization = Column(String)
+
+    buildingblock = relationship('BuildingBlockBase', back_populates='bottles')
+    # TODO are these relationships a problem? Do I need to overwrite them in child classes?
+
+    __mapper_args__ = {
+        'polymorphic_on':       customization,
+        'polymorphic_identity': 'bottlebase'
+    }
+
+    def __repr__(self):
+        return f'Bottle(id={self.id}, barcode={self.barcode}, buildingblock={self.buildingblock})'
---
+++ src/db_retrieval/show_desired_product_by_well.py
@@ -0,0 +1,36 @@
+import sqlite3
+from rdkit import Chem
+from rdkit.Chem import Draw
+from config import DB_PATH
+
+
+def get_product(lab_journal_number, well, product_type):
+    global DB_PATH
+    product_mapping = dict(zip('ABCDEFGH', range(1, 9)))
+    product_idx = product_mapping[product_type]
+    # query db
+    con = sqlite3.connect(DB_PATH)
+    cur = con.cursor()
+    result = cur.execute(
+        'SELECT long_name, product_A_smiles, product_B_smiles, product_C_smiles, product_D_smiles, product_E_smiles, product_F_smiles, product_G_smiles, product_H_smiles FROM experiments WHERE lab_journal_number = ? AND well = ?;',
+        (lab_journal_number, well)).fetchall()
+    if len(result) == 0:
+        print('Error: No entry in DB for the given parameters')
+    long_name = result[0][0]
+    smiles = result[0][product_idx]
+    print(long_name)
+    print(smiles)
+    if smiles is not None:
+        Draw.MolToImage(Chem.MolFromSmiles(smiles), size=(500, 500)).show()
+    else:
+        print('This molecule does not exist')
+
+    con.close()
+    return
+
+
+if __name__ == '__main__':
+    lab_journal_number = 'JG228'
+    well = 'M1'
+    product = 'A'
+    get_product(lab_journal_number, well, product)

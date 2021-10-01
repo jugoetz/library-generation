9b1c5dc29dba1053218269f599a8b2c57d4efdd6
jugoetz <70372980+jugoetz@users.noreply.github.com>
2021-10-01 13:57:24 +0200

    move db method to db_queries.py + refactor

--- src/db_retrieval/db_queries.py
+++ src/db_retrieval/db_queries.py
@@ -19,23 +19,30 @@ class MyDatabaseConnection:
 
     """Some functions for simple queries to the db"""
 
-    def get_mol(self, short):
-        smiles = self.cur.execute('SELECT SMILES FROM main.buildingblocks WHERE short = ?;', (short,)).fetchone()[0]
-        return MolFromSmiles(smiles)
-
-    def get_smiles(self, short):
+    def get_smiles(self, short: str):
+        """Get SMILES from a building block short"""
         smiles = self.cur.execute('SELECT SMILES FROM main.buildingblocks WHERE short = ?;', (short,)).fetchone()[0]
         return smiles
 
-    def show_image(self, short):
+    def get_mol(self, short: str):
+        """Get MOL from a building block short"""
+        return MolFromSmiles(self.get_smiles(short))
+
+    def show_image(self, short: str):
+        """Show the molecular structure drawing for a building block short"""
         img_path = self.cur.execute('SELECT image FROM main.buildingblocks WHERE short = ?;', (short,)).fetchone()[0]
         Image.open(img_path).show()
         return
 
-    def list_pg(self, short):
+    def list_pg(self, short: str):
         """Returns a 4-tuple: (#boc, #cbz, #tbu, #tms)"""
         return self.cur.execute('SELECT boc, cbz, tbu, tms FROM main.buildingblocks WHERE short = ?;',
                                 (short,)).fetchone()
+
+    def get_reactant_class(self, short: str):
+        """Takes a building block short name and returns the reactant class"""
+        return self.cur.execute('SELECT reactant_class FROM main.buildingblocks WHERE short = ?;', (short,)).fetchone()[
+            0]
 
     def get_vl_member(self, vl_id):
         """Takes a vl_id and returns the product MOL"""
@@ -43,10 +50,25 @@ class MyDatabaseConnection:
                                   (vl_id,)).fetchone()[0]
         return MolFromSmiles(smiles)
 
-    def get_reactant_class(self, short):
-        """Takes a building block short name and returns the reactant class"""
-        return self.cur.execute('SELECT reactant_class FROM main.buildingblocks WHERE short = ?;', (short,)).fetchone()[
-            0]
+    def get_product_mol_by_well(self, lab_journal_number: str, well: str, product_type: str):
+        """
+        :param lab_journal_number: Unique identifier for the plate
+        :param well: Identifier for the well within the plate
+        :param product_type: [A-H]
+        :return: rdkit mol object, or None if no match was found
+        """
+        product_mapping = dict(zip('ABCDEFGH', range(8)))
+        product_idx = product_mapping[product_type]
+        result = self.cur.execute(
+            'SELECT product_A_smiles, product_B_smiles, product_C_smiles, product_D_smiles, product_E_smiles, \
+            product_F_smiles, product_G_smiles, product_H_smiles \
+            FROM experiments WHERE lab_journal_number = ? AND well = ?;',
+            (lab_journal_number, well)).fetchall()
+        try:
+            smiles = result[0][product_idx]
+        except KeyError:
+            return None
+        return MolFromSmiles(smiles)
 
     def __delete__(self, instance):
         self.con.close()
--- src/db_retrieval/show_desired_product_by_well.py
+++ src/db_retrieval/show_desired_product_by_well.py
@@ -1,37 +1,10 @@
-import sqlite3
-
-from rdkit import Chem
 from rdkit.Chem import Draw
 
-from definitions import DB_PATH
-
-
-def get_product(lab_journal_number, well, product_type):
-    product_mapping = dict(zip('ABCDEFGH', range(1, 9)))
-    product_idx = product_mapping[product_type]
-    # query db
-    con = sqlite3.connect(DB_PATH)
-    cur = con.cursor()
-    result = cur.execute(
-        'SELECT long_name, product_A_smiles, product_B_smiles, product_C_smiles, product_D_smiles, product_E_smiles, product_F_smiles, product_G_smiles, product_H_smiles FROM experiments WHERE lab_journal_number = ? AND well = ?;',
-        (lab_journal_number, well)).fetchall()
-    if len(result) == 0:
-        print('Error: No entry in DB for the given parameters')
-    long_name = result[0][0]
-    smiles = result[0][product_idx]
-    print(long_name)
-    print(smiles)
-    if smiles is not None:
-        Draw.MolToImage(Chem.MolFromSmiles(smiles), size=(500, 500)).show()
-    else:
-        print('This molecule does not exist')
-
-    con.close()
-    return
-
+from db_queries import MyDatabaseConnection
 
 if __name__ == '__main__':
-    lab_journal_number = 'JG264'
-    well = 'A5'
-    product = 'A'
-    get_product(lab_journal_number, well, product)
+    lab_journal_number = 'JG263'
+    well = 'H15'
+    product = 'G'
+    Draw.MolToImage(MyDatabaseConnection().get_product_mol_by_well(lab_journal_number, well, product),
+                    size=(500, 500)).show()

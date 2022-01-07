990bf19aa4790599e4ca18f84090ed472664057e
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-01-07 14:43:55 +0100

    add a small script to list the number of synthesized compounds by date

---
+++ src/util/number_of_synthesized_compounds.py
@@ -0,0 +1,20 @@
+"""Quick analysis upon request from Bertran
+
+Saves a CSV file that contains the synthesis dates with the number of compounds synthesised on that date.
+"""
+import datetime
+
+import pandas as pd
+
+from src.db_retrieval.db_queries import MyDatabaseConnection
+from definitions import UTIL_DIR
+
+con = MyDatabaseConnection()
+
+dates = con.execute_arbitrary_simple_query("SELECT synthesis_date_unixepoch FROM experiments;")
+dates = [datetime.datetime.fromtimestamp(d[0]) for d in dates]
+df = pd.DataFrame(dates)
+df.value_counts().sort_index().to_csv(UTIL_DIR / "experiment_numbers.csv",
+                                      header=["number of compounds"],
+                                      index_label="synthesis date",
+                                      )

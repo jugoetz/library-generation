c9c1e128002de2039b602f2c59d7f08edbe28f5c
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-03-07 18:28:16 +0100

    add transfers to lcms plate

--- src/library_design/generate_source_transfer_files.py
+++ src/library_design/generate_source_transfer_files.py
@@ -61,6 +61,7 @@ target_plate.from_csv(test_file)
 
 transfers = defaultdict(lambda: ([], []))
 
+# set up transfers from target plate layout
 for well in target_plate.wells():
     well_content = target_plate.well(well)
     if well_content[1] == 0:
@@ -74,5 +75,26 @@ df = pd.DataFrame.from_dict(transfers, orient="index").reset_index().rename(
 df["source_well"] = df["compound"].apply(compound_to_well)
 df["source_plate"] = df["compound"].apply(compound_to_source_plate)
 df["step"] = df["source_plate"].apply(lambda x: 2 if x == "terminator" else 1)
+
+# set up transfers for LCMS analysis plate
+# logic: for all building blocks -> 1 uL into next well of lcms plate,
+# where next well goes in the order A1->D1->A8->D8, and counter is reset for terminators
+# practically, we only need to copy the dataframe for I,M,T transfers and adjust target wells and volumes
+df_lcms = df.loc[df["source_plate"] != "oxalic acid"].copy()
+df_lcms["source_plate"] += "_lcms"
+# chained sort to get I1->I99->M1->M99->T1->T99 order
+df_lcms = df_lcms.sort_values(
+    by="compound", kind="stable", key=lambda x: [int(y[1:]) for y in x]).sort_values(
+    by="compound", kind="stable", key=lambda x: [y[0] for y in x])
+
+df_lcms.loc[df_lcms["source_plate"] == "initiator_lcms", "target_wells"] = [f"{r}{c}" for c in range(1, 5) for r in
+                                                                            "ABCD"]
+df_lcms.loc[df_lcms["source_plate"] == "monomer_lcms", "target_wells"] = [f"{r}{c}" for c in range(5, 8) for r in
+                                                                          "ABCD"]
+df_lcms.loc[df_lcms["source_plate"] == "terminator_lcms", "target_wells"] = [f"{r}{c}" for c in range(1, 4) for r in
+                                                                             "ABCD"][:-2]
+df_lcms["target_volumes"] = [1000, ] * len(df_lcms)
+
+df = pd.concat((df, df_lcms), ignore_index=True)
 
 df[["step", "source_plate", "source_well", "target_wells", "target_volumes"]].to_csv(test_output, index=False)

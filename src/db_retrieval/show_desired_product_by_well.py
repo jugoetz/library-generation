e05e7cd80865b25702a63ebe45a141316d5039b8
jugoetz <70372980+jugoetz@users.noreply.github.com>
2022-09-27 10:35:22 +0200

    add argparsing

--- src/db_retrieval/show_desired_product_by_well.py
+++ src/db_retrieval/show_desired_product_by_well.py
@@ -1,18 +1,30 @@
+import argparse
+
 from rdkit.Chem import Draw, MolToSmiles
 
 from src.db_retrieval.db_queries import MyDatabaseConnection
 
-con = MyDatabaseConnection()
+defaults = {'lab_journal_nr': 'JG290',
+            'well': 'A3',
+            'product_type': 'A',
+            'output': 'smiles',
+            }
 
 if __name__ == '__main__':
-    lab_journal_number = 'JG276'
-    well = 'A5'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lab_journal_number', type=str, help='Lab journal number for the plate',
+                        default=defaults['lab_journal_nr'])
+    parser.add_argument('--well', type=str, help='Well to show the product for', default=defaults['well'])
+    parser.add_argument('--product_type', type=str, help='Product type to show', default=defaults['product_type'],
+                        choices=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+    parser.add_argument('--output', type=str, help='Output type', default=defaults['output'],
+                        choices=['smiles', 'image'])
+    args = parser.parse_args()
 
-    product = 'A'
-    how = 'smiles'
+    con = MyDatabaseConnection()
 
-    if how == 'drawing':
-        Draw.MolToImage(con.get_product_mol_by_well(lab_journal_number, well, product),
+    if args.output == 'image':
+        Draw.MolToImage(con.get_product_mol_by_well(args.lab_journal_number, args.well, args.product_type),
                         size=(500, 500)).show()
-    elif how == 'smiles':
-        print(MolToSmiles(con.get_product_mol_by_well(lab_journal_number, well, product)))
+    elif args.output == 'smiles':
+        print(MolToSmiles(con.get_product_mol_by_well(args.lab_journal_number, args.well, args.product_type)))
